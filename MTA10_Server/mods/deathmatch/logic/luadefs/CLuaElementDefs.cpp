erikbroo/multitheftauto

commit f21e614fad73344a3b008071a51cd30b98190785

Fixed #4028: setElementAttachedOffsets does not exist server side
git-svn-id: http://multitheftauto.googlecode.com/svn/trunk@1411 915fb2f0-b7ca-11dd-a720-8bda5143708b

MTA10_Server/mods/deathmatch/logic/luadefs/CLuaElementDefs.cpp
@@ -891,6 +891,72 @@ int CLuaElementDefs::getAttachedElements ( lua_State* luaVM )
 }
 
 
+int CLuaElementDefs::setElementAttachedOffsets ( lua_State* luaVM )
+{
+    if ( lua_type ( luaVM, 1 ) == LUA_TLIGHTUSERDATA )
+    {
+        // Grab the element to attach and the target
+        CElement* pElement = lua_toelement ( luaVM, 1 );
+        CVector vecPosition, vecRotation;
+
+        // Grab the supplied arguments (pos: x y z, rot: x y z)
+        int iArgument2 = lua_type ( luaVM, 2 );
+        if ( iArgument2 == LUA_TSTRING || iArgument2 == LUA_TNUMBER )
+        {
+            vecPosition.fX = static_cast < float > ( lua_tonumber ( luaVM, 2 ) );
+
+            int iArgument3 = lua_type ( luaVM, 3 );
+            if ( iArgument3 == LUA_TSTRING || iArgument3 == LUA_TNUMBER )
+            {
+                vecPosition.fY = static_cast < float > ( lua_tonumber ( luaVM, 3 ) );
+
+                int iArgument4 = lua_type ( luaVM, 4 );
+                if ( iArgument4 == LUA_TSTRING || iArgument4 == LUA_TNUMBER )
+                {
+                    vecPosition.fZ = static_cast < float > ( lua_tonumber ( luaVM, 4 ) );
+
+                    int iArgument5 = lua_type ( luaVM, 5 );
+                    if ( iArgument5 == LUA_TSTRING || iArgument5 == LUA_TNUMBER )
+                    {
+                        vecRotation.fX = static_cast < float > ( lua_tonumber ( luaVM, 5 ) );
+
+                        int iArgument6 = lua_type ( luaVM, 6 );
+                        if ( iArgument6 == LUA_TSTRING || iArgument6 == LUA_TNUMBER )
+                        {
+                            vecRotation.fY = static_cast < float > ( lua_tonumber ( luaVM, 7 ) );
+
+                            int iArgument7 = lua_type ( luaVM, 7 );
+                            if ( iArgument7 == LUA_TSTRING || iArgument7 == LUA_TNUMBER )
+                            {
+                                vecRotation.fZ = static_cast < float > ( lua_tonumber ( luaVM, 7 ) );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Valid element?
+        if ( pElement )
+        {
+            if ( CStaticFunctionDefinitions::SetElementAttachedOffsets ( pElement, vecPosition, vecRotation ) )
+            {
+                lua_pushboolean ( luaVM, true );
+                return 1;
+            }
+        }
+        else
+            m_pScriptDebugging->LogBadPointer ( luaVM, "setElementAttachedOffsets", "element", 1 );
+    }
+    else
+        m_pScriptDebugging->LogBadType ( luaVM, "setElementAttachedOffsets" );
+
+    // Failed
+    lua_pushboolean ( luaVM, false );
+    return 1;
+}
+
+
 int CLuaElementDefs::getElementColShape ( lua_State* luaVM )
 {
     if ( lua_type ( luaVM, 1 ) == LUA_TLIGHTUSERDATA )

MTA10/mods/deathmatch/logic/rpc/CElementRPCs.cpp
@@ -12,24 +12,26 @@
 
 #include "StdInc.h"
 #include "CElementRPCs.h"
+#include "net/SyncStructures.h"
 
 using std::list;
 
 void CElementRPCs::LoadFunctions ( void )
 {
-    AddHandler ( SET_ELEMENT_PARENT, SetElementParent, "SetElementParent" );
-    AddHandler ( SET_ELEMENT_DATA, SetElementData, "SetElementData" );
-    AddHandler ( REMOVE_ELEMENT_DATA, RemoveElementData, "RemoveElementData" );
-    AddHandler ( SET_ELEMENT_POSITION, SetElementPosition, "SetElementPosition" );
-    AddHandler ( SET_ELEMENT_VELOCITY, SetElementVelocity, "SetElementVelocity" );
-    AddHandler ( SET_ELEMENT_INTERIOR, SetElementInterior, "SetElementInterior" );
-    AddHandler ( SET_ELEMENT_DIMENSION, SetElementDimension, "" );
-    AddHandler ( ATTACH_ELEMENTS, AttachElements, "AttachElements" );
-    AddHandler ( DETACH_ELEMENTS, DetachElements, "DetachElements" );
-    AddHandler ( SET_ELEMENT_ALPHA, SetElementAlpha, "SetElementAlpha" );
-	AddHandler ( SET_ELEMENT_NAME, SetElementName, "SetElementName" );
-    AddHandler ( SET_ELEMENT_HEALTH, SetElementHealth, "SetElementHealth" );
-    AddHandler ( SET_ELEMENT_MODEL, SetElementModel, "SetElementModel" );
+    AddHandler ( SET_ELEMENT_PARENT,            SetElementParent,           "SetElementParent" );
+    AddHandler ( SET_ELEMENT_DATA,              SetElementData,             "SetElementData" );
+    AddHandler ( REMOVE_ELEMENT_DATA,           RemoveElementData,          "RemoveElementData" );
+    AddHandler ( SET_ELEMENT_POSITION,          SetElementPosition,         "SetElementPosition" );
+    AddHandler ( SET_ELEMENT_VELOCITY,          SetElementVelocity,         "SetElementVelocity" );
+    AddHandler ( SET_ELEMENT_INTERIOR,          SetElementInterior,         "SetElementInterior" );
+    AddHandler ( SET_ELEMENT_DIMENSION,         SetElementDimension,        "SetElementDimension" );
+    AddHandler ( ATTACH_ELEMENTS,               AttachElements,             "AttachElements" );
+    AddHandler ( DETACH_ELEMENTS,               DetachElements,             "DetachElements" );
+    AddHandler ( SET_ELEMENT_ALPHA,             SetElementAlpha,            "SetElementAlpha" );
+	AddHandler ( SET_ELEMENT_NAME,              SetElementName,             "SetElementName" );
+    AddHandler ( SET_ELEMENT_HEALTH,            SetElementHealth,           "SetElementHealth" );
+    AddHandler ( SET_ELEMENT_MODEL,             SetElementModel,            "SetElementModel" );
+    AddHandler ( SET_ELEMENT_ATTACHED_OFFSETS,  SetElementAttachedOffsets,  "SetElementAttachedOffsets" );
 }
 
 
@@ -468,6 +470,22 @@ void CElementRPCs::SetElementModel ( NetBitStreamInterface& bitStream )
                     break;
                 }
             }
+        }
+    }
+}
+
+
+void CElementRPCs::SetElementAttachedOffsets ( NetBitStreamInterface& bitStream )
+{
+    ElementID ID;
+    SPositionSync position ( true );
+    SRotationDegreesSync rotation ( true );
+    if ( bitStream.Read ( ID ) && position.Read ( bitStream ) && rotation.Read ( bitStream ) )
+    {
+        CClientEntity* pEntity = CElementIDs::GetElement ( ID );
+        if ( pEntity )
+        {
+            pEntity->SetAttachedOffsets ( position.data.vecPosition, rotation.data.vecRotation );
         }
     }
 }

MTA10/mods/deathmatch/logic/rpc/CElementRPCs.h
@@ -33,6 +33,7 @@ public:
 	DECLARE_RPC ( SetElementName );
     DECLARE_RPC ( SetElementHealth );
     DECLARE_RPC ( SetElementModel );
+    DECLARE_RPC ( SetElementAttachedOffsets );
 };
 
 #endif

MTA10_Server/mods/deathmatch/logic/CStaticFunctionDefinitions.cpp
@@ -1382,6 +1382,32 @@ bool CStaticFunctionDefinitions::SetElementModel ( CElement* pElement, unsigned 
 }
 
 
+bool CStaticFunctionDefinitions::SetElementAttachedOffsets ( CElement* pElement, CVector & vecPosition, CVector & vecRotation )
+{
+    RUN_CHILDREN SetElementAttachedOffsets ( *iter, vecPosition, vecRotation );
+
+    CVector vecCurrentPos, vecCurrentRot;
+    pElement->GetAttachedOffsets ( vecCurrentPos, vecCurrentRot );
+    if ( vecPosition != vecCurrentPos || vecRotation != vecCurrentRot )
+    {
+        pElement->SetAttachedOffsets ( vecPosition, vecRotation );
+        
+        SPositionSync position ( true );
+        position.data.vecPosition = vecPosition;
+
+        SRotationDegreesSync rotation ( true );
+        rotation.data.vecRotation = vecRotation;
+
+        CBitStream BitStream;
+        BitStream.pBitStream->Write ( pElement->GetID () );
+        position.Write ( *BitStream.pBitStream );
+        rotation.Write ( *BitStream.pBitStream );
+        m_pPlayerManager->BroadcastOnlyJoined ( CLuaPacket ( SET_ELEMENT_ATTACHED_OFFSETS, *BitStream.pBitStream ) );
+    }
+    return true;
+}
+
+
 bool CStaticFunctionDefinitions::ClearElementVisibleTo ( CElement* pElement )
 {
     RUN_CHILDREN ClearElementVisibleTo ( *iter );

MTA10_Server/mods/deathmatch/logic/CStaticFunctionDefinitions.h
@@ -89,6 +89,7 @@ public:
     static bool                 SetElementAlpha                     ( CElement* pElement, unsigned char ucAlpha );
     static bool                 SetElementHealth                    ( CElement* pElement, float fHealth );
     static bool                 SetElementModel                     ( CElement* pElement, unsigned short usModel );
+    static bool                 SetElementAttachedOffsets           ( CElement* pElement, CVector & vecPosition, CVector & vecRotation );
 
     // Scoreboard
     static bool                 AddScoreboardColumn                 ( const char* szID, const char* szName, float fWidth );

MTA10_Server/mods/deathmatch/logic/luadefs/CLuaElementDefs.h
@@ -67,10 +67,11 @@ public:
 
     // Attachement
     static int      attachElements                      ( lua_State* luaVM );
-    static int      detachElements            ( lua_State* luaVM );
+    static int      detachElements                      ( lua_State* luaVM );
     static int      isElementAttached                   ( lua_State* luaVM );
     static int      getAttachedElements                 ( lua_State* luaVM );
     static int      getElementAttachedTo                ( lua_State* luaVM );
+    static int      setElementAttachedOffsets           ( lua_State* luaVM );
 
     // Set
     static int      setElementID                        ( lua_State* luaVM );

Shared/sdk/net/rpc_enums.h
@@ -34,6 +34,7 @@ enum eRPCFunctions
 	SET_ELEMENT_NAME,
     SET_ELEMENT_HEALTH,
     SET_ELEMENT_MODEL,
+    SET_ELEMENT_ATTACHED_OFFSETS,
     
     SET_PLAYER_MONEY,            
     SHOW_PLAYER_HUD_COMPONENT,

commit 2e73c6f8c704df1e12f96cdfff1069d6433e4f02

Fix for wrong argument parse introduced in r1411.

MTA10_Server/mods/deathmatch/logic/luadefs/CLuaElementDefs.cpp
@@ -923,7 +923,7 @@ int CLuaElementDefs::setElementAttachedOffsets ( lua_State* luaVM )
                         int iArgument6 = lua_type ( luaVM, 6 );
                         if ( iArgument6 == LUA_TSTRING || iArgument6 == LUA_TNUMBER )
                         {
-                            vecRotation.fY = static_cast < float > ( lua_tonumber ( luaVM, 7 ) );
+                            vecRotation.fY = static_cast < float > ( lua_tonumber ( luaVM, 6 ) );
 
                             int iArgument7 = lua_type ( luaVM, 7 );
                             if ( iArgument7 == LUA_TSTRING || iArgument7 == LUA_TNUMBER )